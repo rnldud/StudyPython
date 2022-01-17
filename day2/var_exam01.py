e9e1ef2896b60138e11f562756c140092f991ea5
rnldud <rnldud97@naver.com>
2022-01-17 17:47:08 +0900

    20220117 월요일 파이썬 학습 완료

---
+++ day2/var_exam01.py
@@ -0,0 +1,56 @@
+#변수 - 파이썬의 변수는 제약이 거의 없다
+
+a = '헬로우'
+print(a)
+
+a = 3.141592
+print(a)
+
+a = 10
+print(a)
+
+
+a = 999999999999
+print(a)
+
+a = 1 / 10
+print(a)
+
+# 변수값을 지정(할당) - assign 방법
+a = 3
+b = 3.141592
+c = 'python'
+d = (1,2,3) # 튜플
+e = [1,2,3,4] # 리스트
+f = [7, '8', '$', a] # 파이썬의 장점!!
+
+#변수명 
+val = 10 
+val2 = 20
+val4 = 30
+#4val = 30
+#-val = 50
+#*val = 60
+val_of_change = 99
+chain_reaction = 109
+#chain Reaction = 109
+#val- = 111
+#val$ = 99
+val_ = 9999
+Val_ = 9080
+print(val_)
+print(Val_)
+MyAccountOfBank = 1
+은행계좌금액 = 1
+
+
+print(id(val_))
+print(id(Val_))
+
+#변수타입 확인
+print(type(val_)) #int
+print(type(c))  #str
+print(type(d))  #tuple
+print(type(e))  #list
+print(type(f))  #list
+
